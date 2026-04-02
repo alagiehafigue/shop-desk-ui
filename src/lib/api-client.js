bb9d51ad6e38d2da28bfa253017beb82420e6642
alagiehafigue <alagiehafigue@gmail.com>
2026-04-02 00:48:52 +0000

    prepare for deployment

--- src/lib/api-client.js
+++ src/lib/api-client.js
@@ -6,7 +6,10 @@ import {
   setStoredAccessToken,
 } from "./storage";
 
-const baseURL = import.meta.env.VITE_API_URL ?? "http://localhost:5000";
+const baseURL = (import.meta.env.VITE_API_URL ?? "http://localhost:5000").replace(
+  /\/$/,
+  "",
+);
 
 export const apiClient = axios.create({
   baseURL,
